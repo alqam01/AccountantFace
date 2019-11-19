b1f4b649f9650e033044ad08e68eb54d450a639d
Mahmoud Alqam <mahmoud-alqam@bayt.net>
2019-11-19 22:20:23 +0200

    NetBeans final test commit

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ echo "Hello world!";
 echo "<h1>This code to test office machine commits</h1>";
 
 echo "<h1>This code to test integration with NetBeans</h1>";
+
+echo 'Yes, We are clear to gooooooo!!';
 ?>
